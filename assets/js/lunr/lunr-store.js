2de8fd7740a810358261911f7117ba7cc7db3e59
martinemre <martinemre@users.noreply.github.com>
2026-04-05 11:11:36 +0000

    deploy: martinemre/devcode@ee9e78160d91abae99eef54f4d0d83d3bfe431a6

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,14 +1,14 @@
 var store = [{
-        "title": "Azure VNet Nedir?",
-        "excerpt":"Bu benim ilk yazımmm  ","categories": ["Azure"],
-        "tags": ["vnet","networking"],
-        "url": "/azure/test1/",
+        "title": "Test",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/test1/",
         "teaser": null
       },{
-        "title": "Azure VNets Nedir?",
-        "excerpt":"Bu benim ilk yazımmm  ","categories": ["AzurE"],
-        "tags": ["vnet","networking"],
-        "url": "/azure/test/",
+        "title": "Test",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/test/",
         "teaser": null
       },{
     "title": "Page 4",
@@ -27,7 +27,7 @@ var store = [{
     "excerpt":"                          ","url": "https://martinemre.github.io/collection-archive/"
   },{
     "title": null,
-    "excerpt":"  \t \t\tmartinemre \t\t**\t\t \t\thttps://martinemre.github.io \t\t \t\t \t\t\t \t\t\t\tAzure VNets Nedir? \t\t\t\t&lt;p&gt;Bu benim ilk yazımmm&lt;/p&gt;  \t\t\t\tSat, 04 Apr 2026 00:00:00 +0000 \t\t\t\thttps://martinemre.github.io/azure/test/ \t\t\t\thttps://martinemre.github.io/azure/test/ \t\t\t \t\t \t\t\t \t\t\t\tAzure VNet Nedir? \t\t\t\t&lt;p&gt;Bu benim ilk yazımmm&lt;/p&gt;  \t\t\t\tMon, 02 Feb 2026 00:00:00 +0000 \t\t\t\thttps://martinemre.github.io/azure/test1/ \t\t\t\thttps://martinemre.github.io/azure/test1/ \t\t\t \t\t \t  ","url": "https://martinemre.github.io/feed.xml"
+    "excerpt":"  \t \t\tmartinemre \t\t**\t\t \t\thttps://martinemre.github.io \t\t \t\t \t\t\t \t\t\t\tTest \t\t\t\t  \t\t\t\tMon, 02 Feb 2026 00:00:00 +0000 \t\t\t\thttps://martinemre.github.io/test/ \t\t\t\thttps://martinemre.github.io/test/ \t\t\t \t\t \t\t\t \t\t\t\tTest \t\t\t\t  \t\t\t\tMon, 02 Feb 2026 00:00:00 +0000 \t\t\t\thttps://martinemre.github.io/test1/ \t\t\t\thttps://martinemre.github.io/test1/ \t\t\t \t\t \t  ","url": "https://martinemre.github.io/feed.xml"
   },{
     "title": null,
     "excerpt":"","url": "https://martinemre.github.io/"
